36c88287fe14a39cef7cf56d0d5db1680bac6934
@Guesmamine <amineguesmi219@gmail.com>
2024-05-23 09:04:23 +0100

    done

--- index.js
+++ index.js
@@ -17,23 +17,26 @@ $('#myBtn').click(function(){
 //    var y = $('').addClass("rules")
 //    var p = $('').addClass('paragraph')
    $("#motherDiv").append(`
-   <div class= "rules">
-   <p class = "paragraph"> Objective: Shiritori is a Japanese word game where players take turns saying words that begin with the final kana (or character) of the previous word.
+   <div class= "rules" style="font-size: 35px;">
+  <Body style="background-image: url(https://64.media.tumblr.com/0901013c2121ffe2b48a755080bf4523/c606e4a7979684c2-78/s1280x1920/e668408582fde369ac25cfa2fbec4451d66f9673.gif); background-size: 1300px;"></Body>
 
-   Word Selection: Players can only use nouns (or words that can be used as nouns) in the game.
+      <h1 id="game-rules" style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;font-size: 50px;text-align: center;font-family: fantasy;color: aliceblue;">Game Rules</h1>
+      <p class = "paragraph;" style="background-color: rgba(73, 70, 70, 0.281);font-size: 45px; border-radius: 35px;text"  > Objective: Shiritori is a Japanese word game where players take turns saying words that begin with the final kana (or character) of the previous word.
    
-   Word Validity: Words must be valid and not repeated. Additionally, conjugations or particles can't be used as standalone words.
+     Word Selection: Players can only use nouns (or words that can be used as nouns) in the game.
+      
+      Word Validity: Words must be valid and not repeated. Additionally, conjugations or particles can't be used as standalone words.
+      
+      Time Limit: Optionally, a time limit can be set for each turn to add pressure and excitement to the game.
+      
+      Game Over: The game ends when a player cannot come up with a valid word within the time limit, repeats a word that has already been said, or uses a word that ends with "n" (ん) in Japanese, as no words start with this character.
    
-   Time Limit: Optionally, a time limit can be set for each turn to add pressure and excitement to the game.
-   
-   Game Over: The game ends when a player cannot come up with a valid word within the time limit, repeats a word that has already been said, or uses a word that ends with "n" (ん) in Japanese, as no words start with this character.
-
-   Scoring: Points are typically awarded for each valid word played, and the player with the highest score at the end of the game wins.
-   </p>
-   </div>
+      Scoring: Points are typically awarded for each valid word played, and the player with the highest score at the end of the game wins.
+      </p>
+      </div>
+      <div class= "butContainer" style="text-align: center;" > <button id = "start" style="border-radius: 40px;width: 400px ;height: 100px;border-color: rgb(153, 132, 132);background-color:rgb(14, 37, 70);font-size: 60px;font-size: 85px;font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;opacity: 90%;cursor: pointer;">START</button> </div>
    `)
-   var b = $(` <div class= "butContainer"> <button id = "start">START</button> </div>`)
-    x.append(b)
+   
 
     $("#start").click(function(){
     
@@ -43,6 +46,9 @@ $('#game').show()
 
 })
 
+$("#player1").animate({
+  
+});
 
 
 
@@ -52,8 +58,7 @@ $('#game').show()
 
 
 
-
-//********************** */
+//Game javascript class:
 
 
 
